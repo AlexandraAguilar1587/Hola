51b81c17b7fa9229821c340719e6b5b187ea311e
AlexandraAguilar <00164722@uca.edu.sv>
2022-05-05 15:25:38 -0600

    Add: practica 3

---
+++ A.A.cpp/cp3-Alexandra.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    cout<<"Hola mi primer mensaje"; 
+    return 0;
+
+}
---
+++ A.A.cpp/cp3-BeatrizAguilar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //declarando una variable tipo cadena
+    string saludo;
+    cout << "Hola mi primer mensaje";
+    //permite la entrada de datos
+    cin >> saludo;
+    cout << saludo;
+    return 0;
+    
+}
